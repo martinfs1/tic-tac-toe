eababaf3f9747e1cf1d2b1507f8421b805fb6f37
martinfs1 <martinfernando-sandander@hotmail.com>
2023-03-08 20:59:07 -0300

    show the gameboard and try to fill the cells with a mark

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 console.log('Tic Tac Toe');
 
-const name = 'James';
+const Gameboard = (() => {
+  const board = Array(9);
 
-const person = { first: name };
+  const renderBoard = () => {
+    const boardDiv = document.querySelector('.board');
+    board.forEach(() => {
+      const cell = document.createElement('button');
+      boardDiv.appendChild(cell);
+      cell.dataset.id = '1';
+    });
+  };
+  const setCell = () => {
 
-console.log(person);
+  };
+  const getBoard = () => board;
+  return { getBoard, renderBoard, setCell };
+})();
 
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
+const PlayerFactory = (name, mark) => {
+  const sayHello = () => console.log(`Hello ${name} ${mark}`);
+  return { name, mark, sayHello };
 };
+
+const FlowGame = (() => {
+  const playerTurnDiv = document.querySelector('.turn');
+  const board = Gameboard;
+
+  const playerOne = PlayerFactory('Lauri', 'X');
+  const playerTwo = PlayerFactory('Cristian', 'O');
+
+  playerTurnDiv.textContent = playerOne.name || playerTwo.name;
+  console.log(playerOne, playerTwo);
+
+  return {
+    playerOne,
+    playerTwo,
+    getBoard: board.getBoard,
+  };
+})();
+
+const DisplayController = (() => {
+  const game = FlowGame;
+  const renderBoard = Gameboard.renderBoard();
+
+  return { game, renderBoard };
+})();
